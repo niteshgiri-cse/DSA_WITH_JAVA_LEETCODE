ad5e6331e029bd32e78e0c3564628d2e30f44fb2
Nitesh Giri <147243803+niteshgiri-cse@users.noreply.github.com>
2026-08-04 12:44:35 +0530

    Time: 2 ms (81.55%), Space: 46.4 MB (92.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,93 +1,67 @@
-import java.util.*;
-
 class Solution {
-
-    static boolean isSafeToPlace(int rowIndex, int colIndex, int n, char[][] board) {
-        int row = rowIndex;
-        int col = colIndex;
-
-        while (col >= 0) {
-            if (board[row][col] == 'Q') {
+    public boolean isSafe(char[][]board,int rowIndex,int colIndex,int n){
+        int row=rowIndex;
+        int col=colIndex;
+        
+        //for horizontal chacking
+        while(col>=0){
+            if(board[row][col]=='Q'){
                 return false;
             }
-            // row Index me koi change nahi karna h
-            // col index ki value zero tak travel karegi
             col--;
         }
-
-        // check left upper diagnol
-        row = rowIndex;
-        col = colIndex;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') {   // Fixed == instead of =
+        //for upper diagonal checking
+        row=rowIndex;
+        col=colIndex;
+        while(col>=0 && row>=0){
+             if(board[row][col]=='Q'){
                 return false;
             }
-            row = row - 1;
-            col = col - 1;
+            col--;
+            row--;
         }
-
-        //check left lower diagnol
-        row = rowIndex;
-        col = colIndex;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q') {
+        //for lower diagonal checking
+        
+        row=rowIndex;
+        col=colIndex;
+        while(row<n && col>=0){
+              if(board[row][col]=='Q'){
                 return false;
             }
-            row = row + 1;
-            col = col - 1;
+            row++;
+            col--;
         }
+        // else 
+        return true;        
 
-        // else
-        return true;
     }
-
-    static void solve(char[][] board, int n, int colIndex, List<List<String>> ans) {
-
-        // base case
-        if (colIndex >= n) {
-            // iska mtlb-> board pr mereko ek valid arrangement milgyi h
-            // iss valid arrangement ko ans me store karlo
-            List<String> temp = new ArrayList<>();
-
-            for (int i = 0; i < n; i++) {      // Fixed 9 -> 0
+    public void solve(char[][]board,int colIndex,int n, List<List<String>> ans){
+        //base case
+        if(colIndex>=n){
+            List<String> temp=new ArrayList<>();
+            for(int i=0;i<n;i++){
                 temp.add(new String(board[i]));
             }
-
             ans.add(temp);
             return;
         }
-
-        // Ak case main solve karta hu, baaki recursion sombhal lega
-        // current column ke har cell par jaake ya fer current column k har row par jaakar
-        // Queen place karuga an rest recursion ko dedunga solve karne ke liye
-        for (int rowIndex = 0; rowIndex < n; rowIndex++) {
-
-            if (isSafeToPlace(rowIndex, colIndex, n, board)) {   
-
-                board[rowIndex][colIndex] = 'Q';                
-
-                solve(board, n, colIndex + 1, ans);
-
-                // important -> undo ya fer Backtracking wala step
-                board[rowIndex][colIndex] = '.';
+        for(int rowIndex=0;rowIndex<n;rowIndex++){
+            if(isSafe(board,rowIndex,colIndex,n)){
+            board[rowIndex][colIndex]='Q';
+            solve(board,colIndex+1,n,ans);
+            board[rowIndex][colIndex]='.';
             }
         }
+
     }
-
     public List<List<String>> solveNQueens(int n) {
+        char[][] board=new char[n][n];
+        List<List<String>> ans=new ArrayList<>();
+        int colIndex=0;
+        for(int i=0;i<n;i++)
+        Arrays.fill(board[i],'.');
 
-        char[][] board = new char[n][n];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
-        }
-
-        int colIndex = 0;
-
-        List<List<String>> ans = new ArrayList<>();
-
-        solve(board, n, colIndex, ans);
-
+        solve(board,colIndex,n,ans);
         return ans;
     }
 }
