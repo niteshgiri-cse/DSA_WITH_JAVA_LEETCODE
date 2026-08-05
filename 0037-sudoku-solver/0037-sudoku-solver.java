60e2689b042d9a3ddee099a687e75c89a58ac7da
Nitesh Giri <147243803+niteshgiri-cse@users.noreply.github.com>
2026-08-05 23:06:27 +0530

    Time: 251 ms (49.87%), Space: 47 MB (5.11%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,93 +1,73 @@
 class Solution {
-
-    public boolean findEmptyCell(char[][] board, int[] emptyCell) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    //store empty cel ki row
-                    emptyCell[0] = i;
-                    emptyCell[1] = j;
+    public boolean findEmptyCell(char[][]board,int[]emptyCell){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    emptyCell[0]=i;
+                    emptyCell[1]=j;
                     return true;
                 }
             }
         }
+        ///if no cell empty 
         return false;
     }
 
-    public boolean isSafeToPlace(char[][] board, char charValue, int rowIndex, int colIndex) {
-
-        //rules:
-        // check for horizontal and same row
-        for (int col = 0; col < 9; col++) {
-            if (board[rowIndex][col] == charValue) {
+    public boolean isSafe(char[][]board,int value,int rowIndex,int colIndex){
+        // rules to check 
+        //same horizontal line 
+        for(int col=0;col<9;col++){
+            if(board[rowIndex][col]==value){
                 return false;
             }
         }
-
-        //check for vertical and same colum
-        for (int row = 0; row < 9; row++) {
-            if (board[row][colIndex] == charValue) {
+        //same vertical line
+        for(int row=0;row<9;row++){
+             if(board[row][colIndex]==value){
                 return false;
             }
-        }
-
-        // check for 3*3 wall sub box
-        /// formula for find starting index
-        int startRow = rowIndex - rowIndex % 3;
-        int startCol = colIndex - colIndex % 3;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int actualRow = startRow + i;
-                int actualCol = startCol + j;
-
-                if (board[actualRow][actualCol] == charValue) {
+        } 
+        // check inside 3*3 matrix 
+        int row=rowIndex-rowIndex%3;
+        int col=colIndex-colIndex%3;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int startRow=row+i;
+                int startCol=col+j;
+                if(board[startRow][startCol]==value){
                     return false;
                 }
             }
         }
+        // value rakhna possible hai tab
+        return true;
 
-        // safe to place
+    }
+   public boolean solveSudokuHelper(char[][]board){
+
+    int [] emptyCell=new int[2];
+    if(!findEmptyCell(board,emptyCell)){
         return true;
     }
+    int rowIndex=emptyCell[0];
+    int colIndex=emptyCell[1];
 
-    public boolean solveSudokoHelper(char[][] board) {
-
-        int[] emptyCell = new int[2];
-
-        if (!findEmptyCell(board, emptyCell)) {
-            // agar empty nahi hoga tab
-            return true;
-        }
-
-        //if let say i found a empty cell
-        int rowIndex = emptyCell[0];
-        int colIndex = emptyCell[1];
-
-        for (int value = 1; value <= 9; value++) {
-
-            char charValue = (char) (value + '0');
-
-            if (isSafeToPlace(board, charValue, rowIndex, colIndex)) {
-
-                // safe hai to place kar do
-                board[rowIndex][colIndex] = charValue;
-
-                //baki recursion sambhal lega
-                if (solveSudokoHelper(board) == true) {
-                    return true;
-                }
-
-                //agar recursion sovle nahi kar paaya,or waps aa gya
-                //current value ko undo karo or backtracking wala step karo
-                board[rowIndex][colIndex] = '.';
+    for(int value=1;value<=9;value++){
+        char currValue=(char) (value+'0');
+        if(isSafe(board,currValue,rowIndex,colIndex)){
+            board[rowIndex][colIndex]=currValue;
+            if(solveSudokuHelper(board)==true){
+                return true;
             }
+            board[rowIndex][colIndex]='.';
         }
-
-        return false;
     }
+    // sare value check karne ke bad bhi nahi huwa to 
+    return false;
+
+   }
 
     public void solveSudoku(char[][] board) {
-        solveSudokoHelper(board);
+        solveSudokuHelper(board);
     }
 }
